264523e0efd0275354966ed6d8baf51bb68f3270
c-quilo <55235161+c-quilo@users.noreply.github.com>
2021-04-09 10:20:47 +0100

    Parameters for loading data

---
+++ variables.py
@@ -0,0 +1,13 @@
+#######################################
+# Data variables #
+#######################################
+
+# Default File Location of the Fluids Dataset
+directory_data = '/data/'
+
+field_name = 'Velocity'
+#field_name = 'Tracer'
+
+start = 150
+end = 1150
+nsize = 100040
