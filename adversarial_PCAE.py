909b8753c1b982de05fd670850ba89fdabec6243
c-quilo <55235161+c-quilo@users.noreply.github.com>
2021-04-13 14:11:25 +0100

    Adversarial PCAE with GAN or WGAN option

--- adversarial_PCAE.py
+++ adversarial_PCAE.py
@@ -7,6 +7,7 @@ import tensorflow.keras.backend as backend
 import matplotlib.pyplot as plt
 import numpy as np
 from keras import backend
+from keras.layers import Lambda
 from sklearn.model_selection import train_test_split
 from keras.constraints import Constraint
 from keras.initializers import RandomNormal
@@ -14,7 +15,6 @@ from keras.initializers import RandomNormal
 from keras import optimizers
 from keras.utils import np_utils
 import tensorflow.keras as tf
-
 
 class ClipConstraint(Constraint):
     # set clip value when initialized
@@ -28,10 +28,13 @@ class ClipConstraint(Constraint):
     # get the config
     def get_config(self):
         return {'clip_value': self.clip_value}
+
 # clip model weights to a given hypercube
-class GAN():
-    # Implementation of wasserstein loss
-    def __init__(self, directory_data, field_name, npcs, latent_dim, look_back, GANorWGAN):
+class AAE():
+
+    def __init__(self, directory_data, field_name, npcs, initNNodes, latent_dim, GANorWGAN):
+
+        # Wasserstein loss
         def wasserstein_loss(y_true, y_pred):
             return backend.mean(y_true * y_pred)
 
@@ -39,133 +42,135 @@ class GAN():
         self.directory_data = directory_data
         self.latent_dim = latent_dim
         self.npcs = npcs
-        self.look_back = look_back
         self.constraint = 0.01
         self.dropoutNumber = 0.5
         self.alpha = 0.3
-        self.hiddenNodes = 64
-        self.look_back = look_back
+        self.initNNodes = initNNodes
         self.GANorWGAN = GANorWGAN
 
         self.c1_hist = []
         self.c2_hist = []
         self.g_hist = []
 
-        self.optimizer = tf.optimizers.RMSprop(lr=0.00005)
+        self.optimizer = tf.optimizers.Nadam()
 
-        if GANorWGAN == 'WGAN':
+        if self.GANorWGAN == 'WGAN':
             self.loss = wasserstein_loss
-        elif GANorWGAN == 'GAN'
+        elif self.GANorWGAN == 'GAN'
             self.loss = 'binary_crossentropy'
 
-        # Adversarial autoencoder loss
         self.loss_gen = 'mse'
+
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
 
-        # Build the adversarial LSTM
-        self.alstm = self.build_alstm()
+        # Build the encoder and decoder
+        self.generator_encoder = self.build_generator_encoder()
+        self.generator_decoder = self.build_generator_decoder()
 
-        real_input = tf.Input(shape=(1, self.latent_dim))
-        alstm_output = self.alstm(real_input)
-
-        # For the combined model we will only train the generator
+        # Only the generator is trained through the combined model, thus:
         self.discriminator.trainable = False
 
-        # The discriminator takes generated images as input and determines validity
-        discriminator_output = self.discriminator(alstm_output)
+        # Connecting models
+        real_input = tf.Input(shape=self.npcs)
+        encoder_output = self.generator_encoder(real_input)
+        decoder_output = self.generator_decoder(encoder_output)
+        discriminator_output = self.discriminator(encoder_output)
 
-        # The combined model (stacked generator and discriminator)
-        # Trains the generator to fool the discriminator
-        self.combined = tf.Model(real_input, [alstm_output, discriminator_output], name='ALSTM')
+        # The combined model stacks the autoencoder and discriminator
+        # The stacked model has one input and two outputs: the decoded input and the discriminator output
+        self.combined = tf.Model(real_input, [decoder_output, discriminator_output], name = 'AAE')
         self.combined.compile(loss=[self.loss_gen, self.loss], loss_weights=[0.999, 0.001], optimizer=self.optimizer)
 
     def build_discriminator(self):
         init = RandomNormal(stddev=0.02)
         const = ClipConstraint(0.01)
 
-        # Discriminator is a bidirectional LSTM with input latent dimensions and output dim = 1
-        in_disc = tf.Input(shape=(1, self.latent_dim))
-        lstm_1 = tf.layers.Bidirectional(tf.layers.LSTM(self.hiddenNodes, return_sequences=False))(in_disc)
-        dp_1 = tf.layers.Dropout(self.dropoutNumber)(lstm_1)
-        bn_1 = tf.layers.BatchNormalization()(dp_1)
-        rv_1 = tf.layers.RepeatVector(1)(bn_1)
-
-        # Discriminator output
-        disc_output = tf.layers.TimeDistributed(tf.layers.Dense(1, activation='linear'))(rv_1)
-
-        discriminator = tf.Model(in_disc, disc_output)
+        in_disc = tf.Input(shape=(self.latent_dim))
+        disc = tf.layers.LeakyReLU(self.alpha)(in_disc)
+        disc = tf.layers.BatchNormalization()(disc)
+        disc_output = tf.layers.Dense(1, activation='sigmoid')(disc)
+        discriminator = tf.Model(in_disc, disc_output, name='Discriminator')
         discriminator.compile(loss=self.loss, optimizer=self.optimizer)
 
         return discriminator
 
-    def build_alstm(self):
+    def build_generator_encoder(self):
         init = RandomNormal(stddev=0.02)
         init = tf.initializers.RandomNormal(stddev=0.02)
 
-        # BiLSTM generator
-        in_gen = tf.Input(shape=(self.look_back, self.latent_dim))
+        input_enc = tf.Input(shape=self.npcs)
+        nNodes = self.initNNodes
+        flag = 0
+        while nNodes > latent_dim:
+            if flag == 0:
+                enc = tf.layers.Dense(nNodes)(input_enc)
+                flag = 1
+            else:
+                enc = tf.layers.Dense(nNodes)(enc)
+            enc = tf.layers.LeakyReLU(self.alpha)(enc)
+            enc = tf.layers.BatchNormalization()(enc)
+            nNodes = nNodes / 2
+        mu = tf.layers.Dense(latent_dim)(enc)
+        sigma = tf.layers.Dense(latent_dim)(enc)
 
-        lstm_1 = tf.layers.Bidirectional(tf.layers.LSTM(self.hiddenNodes, return_sequences=False))(in_gen)
-        dp_1 = tf.layers.Dropout(self.dropoutNumber)(lstm_1)
-        bn_1 = tf.layers.BatchNormalization()(dp_1)
-        rv_1 = tf.layers.RepeatVector(1)(bn_1)
-        # BiLSTM generator output
-        gen_output = tf.layers.TimeDistributed(tf.layers.Dense(self.latent_dim, activation='linear'))(rv_1)
+        # The latent representation ("fake") in a Gaussian distribution is then compared to the "real" arbitrary Gaussian
+        # distribution fed in the Discriminator
+        latent_repr = tf.layers.Lambda(
+            lambda p: p[0] + backend.random_normal(backend.shape(p[0])) * backend.exp(p[1] / 2))(
+            [mu, sigma])
+        generator_encoder = tf.Model(input_enc, latent_repr, name='Encoder')
+        generator_encoder.summary()
+        return generator_encoder
 
-        alstm = tf.Model(in_gen, gen_output)
-        return alstm
+    def build_generator_decoder(self):
+        init = RandomNormal(stddev=0.02)
+        init = tf.initializers.RandomNormal(stddev=0.02)
+
+        # Input to the decoder is the latent space from the encoder
+        input_dec = tf.Input(shape=self.latent_dim)
+        n = 2 * latent_dim
+        flag = 0
+        while n <= self.initNNodes:
+            if flag == 0:
+                dec = tf.layers.Dense(n)(input_dec)
+                flag = 1
+            else:
+                dec = tf.layers.Dense(n)(dec)
+            dec = tf.layers.LeakyReLU(self.alpha)(dec)
+            dec = tf.layers.BatchNormalization()(dec)
+            n = n * 2
+        output_dec = tf.layers.Dense(self.npcs, activation='tanh')(dec)
+        generator_decoder = tf.Model(input_dec, output_dec, name='Decoder')
+
+        generator_decoder.summary()
+        return generator_decoder
+
 
     def train(self, epochs, batch_size=128, sample_interval=50, n_critic=5):
 
-        # Load principal components and use previously trained Adversarial encoder to obtain latent values
+        # Load and pre process the data
 
         pcs_trun = np.load(self.directory_data + '/' + 'pcs_' + self.field_name + '_' +
                            self.observationPeriod + '.npy')
-        # Load encoder
-        generator_enc = tf.models.load_model(self.directory_data + '/' + 'AAE_MV_generator_encoder_Full_'
-                                             + self.field_name + '_' + str(latent_dim) + '_' + str(100000))
 
         np.random.seed(42)
 
-        # Calculate maximum and minimum to normalize between -1 and +1
         min_ls = np.min(pcs_trun)
         max_ls = np.max(pcs_trun)
         min = -1
         max = +1
 
-        global X_all, y_all, X_train, X_test, y_train, y_test
         def scaler(x, xmin, xmax, min, max):
             scale = (max - min) / (xmax - xmin)
             xScaled = scale * x + min - xmin * scale
             return xScaled
 
-        def lookBack(X, look_back=1):
-            # look_back = 10
-            X_lb = np.empty((X.shape[0] - look_back + 1, look_back, X.shape[1]))
-            # X_test = np.empty((look_back, X_test.shape[1], X_train.shape[0] - look_back + 1))
-            ini = 0
-            fin = look_back
-            for i in range(X.shape[0] - look_back + 1):
-                X_lb[i, :, :] = (X[ini:fin, :])
-                ini = ini + 1
-                fin = fin + 1
-            return X_lb
-
-        # Normalise the values between -1 and +1
         ls_scaled = scaler(pcs_trun, min_ls, max_ls, min, max)
 
-        # Encode the normalised values
-        latent_values = generator_enc.predict(ls_scaled)
+        global X_train, y_train, X_all
 
-        lv_scaled = latent_values
-
-        # Training data
-        # All data is used for training, testing is performed in unseen data
-        X_all = lookBack(lv_scaled[:-1, :], look_back)
-        # Delta latent space with respect to time
-        y_all = lv_scaled[look_back:]-lv_scaled[look_back-1:-1]
-        y_all = np.expand_dims(y_all, 1)
+        X_all = ls_scaled
 
         if self.GANorWGAN == 'WGAN':
             real = -np.ones(batch_size)
@@ -176,54 +181,54 @@ class GAN():
             fake = np.zeros(batch_size)
 
         # Training the model
-
         for epoch in range(epochs):
             c1_tmp, c2_tmp = list(), list()
 
             # Training the discriminator more often than the generator
-
             for _ in range(n_critic):
                 # Randomly selected samples and noise
                 randomIndex = np.random.randint(0, X_all.shape[0], size=batch_size)
-                noise = np.random.normal(0, 1, size=(batch_size, self.look_back, self.latent_dim))
+                noise = np.random.normal(0, 1, size=(batch_size, self.latent_dim))
+                # Select a random batch of input
+                real_seqs = X_all[randomIndex]
 
-                # Select a random batch for input
-                real_input = X_all[randomIndex]
-                real_output = y_all[randomIndex]
-
-                # Generate a batch of new outputs (in the latent space) predicted by the generator
-                alstm_seqs = self.alstm.predict(noise)
+                # Generate a batch of new outputs (in the latent space) predicted by the encoder
+                gen_seqs = self.generator_encoder.predict(real_seqs)
 
                 # Train the discriminator
                 # The arbitrary noise is considered to be a "real" sample
-                d_loss_real = self.discriminator.train_on_batch(real_output, real)
+                d_loss_real = self.discriminator.train_on_batch(noise, real)
                 c1_tmp.append(d_loss_real)
                 # The latent space generated by the encoder is considered a "fake" sample
-                d_loss_fake = self.discriminator.train_on_batch(alstm_seqs, fake)
+                d_loss_fake = self.discriminator.train_on_batch(gen_seqs, fake)
                 c2_tmp.append(d_loss_fake)
-                #d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
             self.c1_hist.append(np.mean(c1_tmp))
             self.c2_hist.append(np.mean(c2_tmp))
 
-            #  Training the generator through the stacked model
-            g_loss = self.combined.train_on_batch(real_input, [real_output, real])
+            # Training the stacked model
+            g_loss = self.combined.train_on_batch(real_seqs, [real_seqs, real])
             self.g_hist.append(g_loss)
-            print("%d [C1 real: %f, C2 fake: %f], [G loss: %f, mse: %f]" % (epoch, self.c1_hist[epoch],
-                                                                            self.c2_hist[epoch],
-                                                                            g_loss[0],
-                                                                            g_loss[1]))
+            print("%d [C1 real: %f, C2 fake: %f], [G loss: %f, mse: %f]" % (epoch, self.c1_hist[epoch], self.c2_hist[epoch], g_loss[0], g_loss[1]))
 
             # Checkpoint progress: Plot losses and predicted data
             if epoch % sample_interval == 0:
 
                 self.plot_loss(epoch)
                 self.plot_values(epoch)
-                # Saving the adversarial LSTM
-                self.alstm.save(self.directory_data + '/' + 'AAE_generator_LSTM_GAN_noise_'
-                                + self.field_name + '_' + str(self.latent_dim) + '_' + str(epoch), save_format='tf')
+                self.generator_encoder.save(self.directory_data + '/' + 'AAE_MV_generator_encoder_Full_WGAN_' +
+                                            self.field_name + '_' + str(self.latent_dim) + '_' + str(epoch),
+                                            save_format='tf')
+                self.generator_decoder.save(self.directory_data + '/' + 'AAE_MV_generator_decoder_Full_WGAN_' +
+                                            self.field_name + '_' + str(self.latent_dim) + '_' + str(epoch),
+                                            save_format='tf')
+
+                self.discriminator.save(self.directory_data + '/' + 'AAE_MV_discriminator_Full_WGAN_' +
+                                        self.field_name + '_' + str(self.latent_dim) + '_' + str(epoch),
+                                        save_format='tf')
 
     # Plots the (W)GAN related losses at every sample interval
+
     def plot_loss(self, epoch):
         fig = plt.figure()
         plt.subplot(1,2,1)
@@ -236,58 +241,52 @@ class GAN():
         plt.legend(['C real', 'C fake', 'Generator'])
 
         plt.subplot(1,2,2)
-        plt.plot(self.g_hist, c='green')
+        plt.subplot(self.g_hist, c='green')
         plt.xlabel('Epoch')
-        plt.ylabel('Mean squared error (-)')
+        plt.ylabel('Mean squared error')
         plt.title('MSE loss')
-        plt.legend(['MSE loss'])
-        plt.savefig(self.directory_data + '/' + 'Losses_AAE-PCAE_LSTM_GAN_noise_' +
-                    self.field_name + '_' + '_' + str(epoch)+ '_' + str(self.latent_dim) +
-                    '.png')
+        plt.savefig(self.directory_data + '/' + 'Losses_AAE_MV-PCAE_WGAN_' + self.field_name + '_' + '_' + str(epoch) +
+                    '_' + str(self.latent_dim) + '.png')
         plt.close()
 
     # Plots predicted in the first 8 latent dimension at every sample interval
-    def plot_values(self, epoch):
-        prediction = self.alstm.predict(X_all)
-        for i in np.arange(8):
-            plt.subplot(2, 4, i+1)
-            plt.plot(y_all[:, i])
-            plt.plot(prediction[:, :, i], alpha=0.8)
-            plt.title('LS dim: ' + str(i))
-        plt.tight_layout()
 
+    def plot_values(self, epoch):
+
+        prediction = self.generator_decoder.predict(self.generator_encoder(X_all))
+        for i in np.arange(12):
+            plt.subplot(3, 4, i+1)
+            plt.plot(X_all[:, i])
+            plt.plot(prediction[:, i], alpha=0.8)
+
+            #plt.legend(['Prediction', 'GT'])
         plt.tight_layout()
-        plt.savefig(self.directory_data + '/' + 'Plots_AAE-PCAE_LSTM_GAN_noise_' +
-                    self.field_name + '_' + '_' + str(epoch) + '_' + str(self.latent_dim) +
-                    '.png')
+        plt.savefig(self.directory_data + '/' + 'Plots_AAE_MV-PCAE_WGAN_' + self.field_name + '_' + '_' + str(epoch) +
+                    '_' + str(self.latent_dim) + '.png')
         plt.close()
 
 if __name__ == '__main__':
     directory_data = '/data/'
     field_name = 'Velocity'
-    npcs = 1000
-    latent_dim = 8
-    start_interv = 150
-    end_interv = 1150
 
-    latentSpaceDimensions = [8]  # , 16, 32, 64, 128]
-    epochs = 100001
+    epochs = 100000
     batch_size = 32
     n_critic = 5
     sample_interval = 10000
-    look_back = 5
-
+    latent_dim = 4
+    npcs = 1000
+    #Initial number of nodes for the AE
+    initNNodes = 64
     #Training method
     GANorWGAN = 'WGAN'
-
-    gan = GAN(directory_data=directory_data,
+    advAE = AAE(directory_data=directory_data,
               field_name=field_name,
               npcs=npcs,
-              latent_dim = latent_dim,
-              look_back=look_back,
+              initNNodes=initNNodes,
+              latent_dim=latent_dim,
               GANorWGAN=GANorWGAN)
 
-    gan.train(epochs=epochs,
+    advAE.train(epochs=epochs,
               batch_size=batch_size,
               sample_interval=sample_interval,
               n_critic = n_critic)
