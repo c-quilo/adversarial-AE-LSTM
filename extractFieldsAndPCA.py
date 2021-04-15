e6a3d1c4c3b055804916ac3fb071c57bc4e1855e
c-quilo <55235161+c-quilo@users.noreply.github.com>
2021-04-15 14:01:11 +0100

    Extract data and perform PCA on selected fields

--- extractFieldsAndPCA.py
+++ extractFieldsAndPCA.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyvista as pv
 import eofs
 from eofs.standard import Eof
+import variables
 
 class extractFieldsAndPCA():
     def __init__(self, directory_data, start, end, start_pca, end_pca, nsize):
@@ -12,7 +13,7 @@ class extractFieldsAndPCA():
         self.end = end
         self.nsize = nsize
 
-        #For PCA analysis specific time-steps
+        # For PCA analysis specific time-steps
         self.start_pca = start_pca
         self.end_pca = end_pca
 
@@ -37,9 +38,9 @@ class extractFieldsAndPCA():
         start_interv = self.start_pca
         end_interv = self.end_pca
         observationPeriod = 'data_' + str(start_interv) + '_to_' + str(end_interv)
-        modelData = np.load(self.directory_data + observationPeriod + '/' + field_name + '_' + observationPeriod + '.npy')
+        modelData = np.load(self.directory_data + '/' + field_name + '_' + observationPeriod + '.npy')
 
-        #Velocity is a 3D vector and needs to be reshaped before the PCA
+        # Velocity is a 3D vector and needs to be reshaped before the PCA
         if 'Velocity' in field_name:
             modelData = np.reshape(modelData, (modelData.shape[0], modelData.shape[1] * modelData.shape[2]), order='F')
 
@@ -51,11 +52,12 @@ class extractFieldsAndPCA():
         #PCA solver
         solver = Eof(modelDataScaled)
 
-        #Principal Components time-series
+        # Principal Components time-series
         pcs = solver.pcs()
-        #Projection
+        # Projection
         eof = solver.eofs()
-
+        # Cumulative variance
+        varianceCumulative = np.cumsum(solver.varianceFraction())
 
         np.save(self.directory_data + '/' + 'pcs_' + field_name + '_' + observationPeriod,
                 pcs)
@@ -63,9 +65,36 @@ class extractFieldsAndPCA():
                 eof)
         np.save(self.directory_data + '/' + 'varCumulative_' + field_name + '_' + observationPeriod,
                 varianceCumulative)
-        np.save(self.directory_data + '/' + 'eigenvalues_' + field_name + '_' + observationPeriod,
-                eigenvalues)
         np.save(self.directory_data + '/' + 'mean_' + field_name + '_' + observationPeriod,
                 meanData)
         np.save(self.directory_data + '/' + 'std_' + field_name + '_' + observationPeriod,
                 stdData)
+
+if __name__ == '__main__':
+    directory_data = '/data/'
+    field_name = 'Velocity'
+
+    # Interval within the simulation for extracting data
+    start = 150
+    end = 1150
+    # Interval within the extracted data to perform PCA
+    start_pca = 150
+    end_pca = 1150
+    # Number of nodes in the unstructured mesh
+    nsize = 100040
+
+    extractFieldsAndPCA = extractFieldsAndPCA(directory_data=directory_data,
+              start=start,
+              end=end,
+              start_pca=start_pca,
+              end_pca=end_pca,
+              nsize=nsize)
+
+    # Extracts data
+    extractFieldsAndPCA.extractFields()
+
+    # PCA on velocity fields
+    extractFieldsAndPCA.PCA(field_name='Velocity')
+
+    # PCA on tracer field
+    extractFieldsAndPCA.PCA(field_name='Tracer')
